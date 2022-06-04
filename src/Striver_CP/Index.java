ff410a184001801d4da93ec74f254b1cb51cb3bf
Anmol Garg <anmolgarg848@gmail.com>
2022-06-04 12:22:37 +0530

    Initializing First Commit of the Repository

---
+++ src/Striver_CP/Index.java
@@ -0,0 +1,10 @@
+package Striver_CP;
+
+import java.util.List;
+
+public class Index {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/Striver_CP/Week1/Day1.java
@@ -0,0 +1,15 @@
+package Striver_CP.Week1;
+
+public class Day1 {
+    public static void main(String[] args) {
+        /*
+        Intro to Cp
+        Time Complexity
+        How Online Judges Work
+        Prefix Sum
+        Two Pointer Technique
+         */
+
+
+    }
+}
